be50c0b70b7ad5e62fd64373827f9705a3658863
infugate <infugate43@gmail.com>
2025-03-19 02:12:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -40,14 +40,14 @@ const titliWinnerRouter = require("./Routes/titliWinnerRoutes.js")
 port = 4000
 // CORS configuration
 
-// app.use(
-//   cors({
-//     origin: ["https://www.98fastbet.com", "https://admin.98fastbet.com"], // Replace '*' with the specific origin(s) you want to allow, e.g., 'https://yourdomain.com'
-//     methods: ['POST', 'GET', 'PUT', 'DELETE'], // Define allowed HTTP methods
-//     credentials: true, // Allow credentials like cookies to be sent
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://reddybok-front.vercel.app", "https://reddybok-admin.vercel.app"], // Replace '*' with the specific origin(s) you want to allow, e.g., 'https://yourdomain.com'
+    methods: ['POST', 'GET', 'PUT', 'DELETE'], // Define allowed HTTP methods
+    credentials: true, // Allow credentials like cookies to be sent
+  })
+);
+// app.use(cors());
 
 const MONGO_URI = process.env.mongodb_url;
 // MongoDB connection
